fb81d9f23a1b18b67ed3d398e77e8a5e3d5565c4
Animesh Kumar <animesh.kumar@successive.com>
2021-10-26 18:10:28 +0530

    Material UI - Styling, Dialog, and Form Validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import { Typography } from '@mui/material';
-import theme from './theme';
-import { ChildrenDemo } from './pages/ChildrenDemo';
-
 // import logo from './logo.svg';
 // import './App.css';
 // import TextFieldDemo from './pages/TextFieldDemo/TextFieldDemo';
-// import InputDemo from './pages/InputDemo/InputDemo';
+import { Trainee } from './pages';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      {/* <div className="App"> */}
+    <div className="App">
       {/* <TextFieldDemo /> */}
-      {/* <InputDemo />
-      </div> */}
-
-      <Typography>
-        <ChildrenDemo />
-      </Typography>
-    </ThemeProvider>
+      {/* <InputDemo /> */}
+      <Trainee />
+    </div>
   );
 }
 
---
+++ src/pages/Trainee/Trainee.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// eslint-disable-next-line import/named
+import { AddDialog } from './Components';
+
+const Trainee = () => (
+  <AddDialog />
+);
+
+export default Trainee;
