9970fb70084deb0de765fcbb0529cada4bd6c147
Animesh Kumar <animesh.kumar@successive.tech>
2021-12-09 17:42:35 +0530

    Feat: Add CssBaseLine

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
 import {
   Trainee, Login, TextFieldDemo, ChildrenDemo, InputDemo, NotFound,
 } from './pages';
@@ -9,17 +10,19 @@ import { PrivateRoute } from './routes';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <PrivateRoute exact path="/" component={Trainee} />
-          <AuthRoute exact path="/login" component={Login} />
-          <PrivateRoute exact path="/trainee" component={Trainee} />
-          <PrivateRoute exact path="/childrendemo" component={ChildrenDemo} />
-          <PrivateRoute exact path="/textfielddemo" component={TextFieldDemo} />
-          <PrivateRoute exact path="/inputdemo" component={InputDemo} />
-          <PrivateRoute component={NotFound} />
-        </Switch>
-      </BrowserRouter>
+      <CssBaseline>
+        <BrowserRouter>
+          <Switch>
+            <PrivateRoute exact path="/" component={Trainee} />
+            <AuthRoute exact path="/login" component={Login} />
+            <PrivateRoute exact path="/trainee" component={Trainee} />
+            <PrivateRoute exact path="/childrendemo" component={ChildrenDemo} />
+            <PrivateRoute exact path="/textfielddemo" component={TextFieldDemo} />
+            <PrivateRoute exact path="/inputdemo" component={InputDemo} />
+            <PrivateRoute component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </CssBaseline>
     </div>
   );
 }
