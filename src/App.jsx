e1a00422f83e306c32a4b7bf8c9b6bd8810ef0fd
Animesh Kumar <animesh.kumar@successive.tech>
2021-12-09 16:04:43 +0530

    feat: SnackBar and Context API

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,24 @@ import {
 import AuthRoute from './routes/AuthRoute';
 import PrivateRoute from './routes/PrivateRoute';
 import TraineeDetail from './pages/Trainee/TraineeDetail';
+import SnackBarProvider from './contexts/SnackBarProvider';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <PrivateRoute exact path="/" component={Trainee} />
-          <AuthRoute exact path="/login" component={Login} />
-          <PrivateRoute exact path="/trainee" component={Trainee} />
-          <PrivateRoute exact path="/childrendemo" component={ChildrenDemo} />
-          <PrivateRoute exact path="/textfielddemo" component={TextFieldDemo} />
-          <PrivateRoute exact path="/inputdemo" component={InputDemo} />
-          <PrivateRoute exact path="/trainee/:id" component={TraineeDetail} />
-          <PrivateRoute component={NotFound} />
-        </Switch>
+        <SnackBarProvider>
+          <Switch>
+            <PrivateRoute exact path="/" component={Trainee} />
+            <AuthRoute exact path="/login" component={Login} />
+            <PrivateRoute exact path="/trainee" component={Trainee} />
+            <PrivateRoute exact path="/childrendemo" component={ChildrenDemo} />
+            <PrivateRoute exact path="/textfielddemo" component={TextFieldDemo} />
+            <PrivateRoute exact path="/inputdemo" component={InputDemo} />
+            <PrivateRoute exact path="/trainee/:id" component={TraineeDetail} />
+            <PrivateRoute component={NotFound} />
+          </Switch>
+        </SnackBarProvider>
       </BrowserRouter>
     </div>
   );
