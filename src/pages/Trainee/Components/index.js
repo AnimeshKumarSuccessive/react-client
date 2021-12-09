fddd12b180d0ea8b99841d24d9bcadb398b81aa3
Animesh Kumar <animesh.kumar@successive.tech>
2021-12-09 15:54:59 +0530

    feat: Pagination in Table

--- src/pages/Trainee/Components/index.js
+++ src/pages/Trainee/Components/index.js
@@ -1,2 +1,4 @@
 export { default as AddDialog } from './AddDialog';
 export { default as TableList } from './TableList';
+export { default as EditDialog } from './EditDialog';
+export { default as RemoveDialog } from './RemoveDialog';
